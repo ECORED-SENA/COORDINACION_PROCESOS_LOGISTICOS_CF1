bd09896accf238ad71bdcfe2b6d762f2de115204
wroaserrano <walter.roas@misena.edu.co>
2021-06-04 07:46:02 -0500

    Ajuste estilos 1
    ajustes estilos _custom

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -41,7 +41,7 @@
           i.fas.fa-quote-right.cab-color
     hr.borde-cab
     p El término “indicador”, en el lenguaje común, se refiere a datos esencialmente cuantitativos, que permiten dar cuenta de cómo se encuentran las cosas en relación con algún aspecto de la realidad que nos interesa conocer. Los Indicadores pueden ser: medidas, números, hechos, opiniones o percepciones que señalen condiciones o situaciones específicas. Estos indicadores deberán reflejarse adecuadamente en la naturaleza, peculiaridades y nexos de los procesos que se originan en la actividad económico-productiva, sus resultados, gastos, entre otros, y caracterizarse por ser estables y comprensibles, por tanto, no es suficiente con uno solo de ellos para medir la gestión de la empresa sino que se impone la necesidad de considerar los sistemas de indicadores, es decir, un conjunto interrelacionado de ellos que abarque la mayor cantidad posible de magnitudes a medir.
-    p Características de los indicadores:
+    p.mb-5 Características de los indicadores:
     .row.align-items-center.mb-5
       .col-lg-6
         .tarjeta-avatar-b.mb-4
@@ -65,6 +65,268 @@
           .tarjeta.fondo-azul-clarito
             .p-4
               .h4.mb-0 Son instrumentos valiosos para determinar cómo se pueden alcanzar mejores resultados en proyectos de desarrollo.
+    p Aparte de sus características fundamentales, se tienen tres aspectos adicionales a tener en cuenta: sus indicadores, saber para qué se requieren y de qué sirve cada uno.
+    AcordionA.mb-5(tipo="b" clase-tarjeta="tarjeta fondo-azul-clarito")
+      .row(titulo="Indicadores logísticos")
+        p Son relaciones de datos numéricos y cuantitativos aplicados a la gestión logística que permiten evaluar el desempeño y el resultado en cada proceso e incluyen los procesos de recepción, almacenamiento, inventarios, despachos, distribución, entregas, facturación y los flujos de información entre los socios de negocios. Es indispensable, entonces, que toda empresa desarrolle habilidades alrededor del manejo de los indicadores de gestión logística, con el fin de poder utilizar la información resultante de manera oportuna (tomar decisiones).
+      div(titulo="Objetivos")
+        ul.lista-ul--separador.mb-5
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Identificar y tomar acciones sobre los problemas operativos.
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Medir el grado de competitividad de la empresa frente a sus competidores nacionales e internacionales
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Satisfacer las expectativas del cliente mediante la reducción del tiempo de entrega y la optimización del servicio prestado.
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Mejorar el uso de los recursos y activos asignados, para aumentar la productividad y efectividad en las diferentes actividades hacia el cliente final.
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Reducir gastos y aumentar la eficiencia operativa.
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Compararse con las empresas del sector en el ámbito local y mundial (Benchmarking)
+      div(titulo="Utilidades")
+        ul.lista-ul--separador.mb-5
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Parametrizador de la planeación de actividades logísticas.
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Medición de resultados.
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Proyección de logros.
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Identificación de mejoras internas.
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Dinamizador de los procesos logísticos de mercancías mediante la interrelación de todas sus actividades internas (armonía).
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Potencializador de la actividad comercial.
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Multiplicador de la realidad empresarial.
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Capacidad real.
+          li 
+            i.fas.fa-angle-right.cab-color
+            p Capacidad instalada.
+    .separador
+    #t_4_4.titulo-segundo-cab.mb-5
+      h2 4.4.	 Medición del desempeño logística de aprovisionamiento
+    p.mb-5 Para el caso de la logística, solo se deben desarrollar indicadores para aquellas actividades o procesos relevantes al objetivo logístico de la empresa y, para lo anterior, se deben tener en cuenta los siguientes pasos:
+    .row.align-items-start.lista-grande.mb-5
+      .col-lg-6
+        ol.lista-ol--circulo.lista-ol--separador
+          li 
+            .lista-ol--circulo__vineta
+              span.text-center 1
+            | Identificar el proceso logístico a medir.
+          li 
+            .lista-ol--circulo__vineta
+              span 2
+            | Conceptualizar cada paso del proceso.
+          li 
+            .lista-ol--circulo__vineta
+              span 3
+            | Definir el objetivo del indicador y cada variable a medir.
+          li 
+            .lista-ol--circulo__vineta
+              span 4
+            | Recolectar información inherente al proceso.
+          li 
+            .lista-ol--circulo__vineta
+              span 5
+            | Cuantificar y medir las variables.
+      .col-lg-6
+        ol.lista-ol--circulo.lista-ol--separador
+          li 
+            .lista-ol--circulo__vineta
+              span 6
+            | Establecer el indicador a controlar.
+          li 
+            .lista-ol--circulo__vineta
+              span 7
+            | Comparar con el indicador global y el de la competencia interna.
+          li 
+            .lista-ol--circulo__vineta
+              span 8
+            | Seguir y retroalimentar las mediciones periódicamente.
+          li 
+            .lista-ol--circulo__vineta
+              span 9
+            | Mejorar continuamente el indicador.
+          li 
+            .lista-ol--circulo__vineta
+              span 10
+            | Proyección y benchmarking externos.
+    h3 Indicadores de compras y abastecimiento.
+    p Estos indicadores están diseñados en función de evaluar y mejorar continuamente la gestión de compras y abastecimiento como factor clave en el éxito de la gestión de la cadena de suministro de la compañía, donde se pueden controlar aspectos del proceso de compras como de las negociaciones y alianzas estratégicas hechas con proveedores.
+    .tabla-a.color-acento-contenido.mb-5 
+      table
+        caption Nota. Adaptada de Mora (2008)
+        thead
+          tr
+            th Indicador
+            th Cálculo
+            th Objetivo
+        tbody
+          tr
+            td Certificación de proveedores.
+            td Proveedores/Total proveedores
+            td Controlar la calidad de los proveedores y el nivel de integración con los mismos.
+          tr
+            td Calidad de los pedidos generados.
+            td Pedidos generados sin problema/Total pedido generados * 100.
+            td Controlar la calidad de los pedidos generados por el área de compras.
+          tr
+            td Volumen de compra.
+            td Valor de compra/Total de las ventas.
+            td Controla la evolución del volumen de compra en relación con el volumen de venta.
+          tr
+            td Entregas recibidas.
+            td Pedidos rechazados/Total órdenes de compra recibidas *100.
+            td Controlar la calidad de los productos/materiales recibidos, y la puntualidad de las entregas de los proveedores de mercancía.
+    h3 Indicadores de almacenamiento y bodegaje.
+    p La gestión de almacenamiento debe estar totalmente alineada con la gestión de aprovisionamiento y distribución, por lo tanto, el control sobre los procesos generados al interior del centro de distribución o almacén es determinante en cuanto al impacto de los costos de operación sobre la operación logística.
+    .tabla-a.color-acento-contenido.mb-5 
+      table
+        caption Nota. Adaptada de Mora (2008)
+        thead
+          tr
+            th Indicador
+            th Cálculo
+            th Objetivo
+        tbody
+          tr
+            td Costo de unidad almacenada.
+            td Costo del almacenamiento/Número de unidades almacenadas.
+            td Controlar el valor unitario del costo por almacenamiento propio o contratado.
+          tr 
+            td Costo de unidad despachada.
+            td Costo de operación bodega/Total unidades despachadas.
+            td Controlar los costos unitarios por manejo de las unidades de carga de la bodega.
+          tr
+            td Nivel de cumplimento en despacho.
+            td Número de despachos cumplidos a tiempo/Número total de despachos requeridos.
+            td Controlar la eficacia de los despachos efectuados por el centro de distribución.
+    .separador
+    #t_4_5.titulo-segundo-cab.mb-5
+      h2 4.5. Medición del desempeño logística interna
+    p Los movimientos de materiales y productos a lo largo de la cadena de suministro son un aspecto clave en la gestión logística, ya que de ello depende el reabastecimiento ó   gística de la empresa.
+    h3 Indicadores de producción e inventario
+    .tabla-a.color-acento-contenido.mb-5 
+      table
+        caption Nota. Adaptada de Mora (2008)
+        thead
+          tr
+            th Indicador
+            th Cálculo
+            th Objetivo
+        tbody
+          tr
+            td Capacidad de producción utilizada.
+            td Capacidad utilizada/Capacidad máxima del recurso.
+            td Controlar la utilización efectiva de las instalaciones (productivas, de almacenaje o transporte) de la compañía.
+          tr
+            td Rendimiento de máquina.
+            td Número de unidades productivas/Capacidad máxima del recurso *100.
+            td Controlar la productividad de una máquina de manufactura con respecto a la capacidad máxima de utilización posible.
+          tr
+            td Rotación de mercancía.
+            td Ventas acumuladas/Inventario promedio.
+            td Controlar la cantidad de los productos/materiales despachados desde el centro de distribución.
+          tr
+            td Duración del inventario.
+            td Inventario final/Ventas promedio *30 días.
+            td Controlar los días de inventario disponible de la mercancía almacenada en el centro de distribución.
+          tr
+            td Vejez del inventario.
+            td Unidades dañadas + obsoletas + vendidas/Unidades disponibles en el inventario.
+            td Controlar el nivel de las mercancías no disponibles para despacho por obsolescencias, mal estado y otros.
+    .separador
+    #t_4_6.titulo-segundo-cab.mb-5
+      h2 4.6.	 Medición del desempeño logística de salida
+    p La distribución es una función logística vital para el desempeño exitoso de la compañía, por lo tanto, es fundamental poder controlar los costos y productividad asociados a la gestión de la misma, particularmente de la gestión del transporte la cual es la actividad que más consume recursos y esfuerzos dentro de la gestión logística por su fuerte impacto en inversiones de activos fijos y respuesta al cliente final.
+    h3 Indicadores de compras y abastecimiento.
+    .tabla-a.color-acento-contenido.mb-5 
+      table
+        caption Nota. Adaptada de Mora (2008)
+        thead
+          tr
+            th Indicador
+            th Cálculo
+            th Objetivo
+        tbody
+          tr
+            td Costo de transporte Vs. venta.	Costo del transporte/Valor ventas totales *100.	Controlar el costo del transporte respecto a las ventas de la empresa.
+          tr
+            td Costo operativo por conductor.
+            td Costo total transporte/Número de conductores.
+            td Controlar la contribución de cada conductor dentro de los gastos totales de transporte.
+          tr
+            td Comparativo costo de transporte.
+            td Costo de transporte propio por unidad/Costo de contratar transporte por unidad.
+            td Controlar los gastos propios de unidades transportadas con los que ofrece el mercado de terceros.
+    .separador
+    #t_4_7.titulo-segundo-cab.mb-5
+      h2 4.7.	Sistemas de recolección de información
+    p Los sistemas de recolección de la información son variados y, en la actualidad, con las tecnologías de la información y comunicación son casi innumerables, a continuación se hace una mención de los sistemas más representativos que se deben considerar en una empresa; se hace la salvedad, que los sistemas relacionados a continuación no son los únicos y que cada empresa debe buscar el que mejor se ajuste a sus necesidades puntuales, pues son muchas las empresas que están trabajando en el desarrollo de estos sistemas y la facilidad de adquisición es alta.
+    .tarjeta-avatar-b
+      img(src='@/assets/curso/tema4/f13.svg', alt='T3-F13', style="width:60px; border-bottom:none;")
+      .tarjeta.transparent1
+        .p-4
+          .h4 MRP
+          p.mb-0 (Material Resource Planning): brinda el soporte específico para propiciar una mayor productividad de las plantas y los principios del Kaizen.
+    .tarjeta-avatar-b
+      img(src='@/assets/curso/tema4/f13.svg', alt='T3-F13', style="width:60px; border-bottom:none;")
+      .tarjeta.transparent1
+        .p-4
+          .h4 MRP
+          p.mb-0 (Warehousing Management System o sistema de administración de inventarios), subsistema de información que ayuda en la administración del flujo del producto y el manejo de las instalaciones en la red logística.
+    .tarjeta-avatar-b
+      img(src='@/assets/curso/tema4/f13.svg', alt='T3-F13', style="width:60px; border-bottom:none;")
+      .tarjeta.transparent1
+        .p-4
+          .h4 TMS
+          p.mb-0 (Transportation Management System o Sistema de Gestión del Transporte), es una solución que posibilita al usuario visualizar, racionalizar, simplificar y controlar diariamente toda la operación y el costo de transporte de una manera integrada.
+    .tarjeta-avatar-b
+      img(src='@/assets/curso/tema4/f13.svg', alt='T3-F13', style="width:60px; border-bottom:none;")
+      .tarjeta.transparent1
+        .p-4
+          .h4 CRM
+          p.mb-0 (Consumer Relationship Management o Administración de Relaciones con el Consumidor), estrategia que permite a las empresas identificar, atraer y retener a sus clientes. El CRM cubre los procesos de mercadeo, ventas y servicio al cliente.
+    .tarjeta-avatar-b
+      img(src='@/assets/curso/tema4/f13.svg', alt='T3-F13', style="width:60px; border-bottom:none;")
+      .tarjeta.transparent1
+        .p-4
+          .h4 ERP
+          p.mb-0 (Enterprise Resource Management o Planificación de Recursos de la Empresa), programas de software concebidos para gestionar de forma integrada las funciones de la empresa, por lo cual, facilita e integra información entre las funciones de manufactura, logística, finanzas y recursos humanos.
+    .tarjeta-avatar-b
+      img(src='@/assets/curso/tema4/f13.svg', alt='T3-F13', style="width:60px; border-bottom:none;")
+      .tarjeta.transparent1
+        .p-4
+          .h4 EDI
+          p.mb-0 (Electronic Data Interchange o Intercambio Electrónico de Datos), permite intercambiar información entre empresas, mediante un formato específico común. Esto hace innecesaria la intervención humana, ya que las operaciones se llevan a cabo íntegramente a través de ordenadores. Con internet, el EDI gana de nuevo protagonismo, en el escenario del B2B (Business to Business) o negocios electrónicos entre empresas.
+    .tarjeta-avatar-b.mb-5
+      img(src='@/assets/curso/tema4/f13.svg', alt='T3-F13', style="width:60px")
+      .tarjeta.transparent2
+        .p-4
+          .h4 GPS
+          p.mb-0 (Global Position System o Sistemas de Localizacion Satelital), localización mundial, por medio de tecnología satelital.
+
+
+
+
+
+
 
 
 </template>
